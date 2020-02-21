6772e0d0c7da1629554f417626eb5b3a09513856
Em Ji Madhu <emjimadhu@gmail.com>
2020-02-21 17:12:27 +0530

    Initial Boilerplate

---
+++ app.ts
@@ -0,0 +1,12 @@
+import { App } from 'alosaur/mod.ts';
+
+import { HomeArea } from './areas/home/home.area.ts';
+import { InfoArea } from './areas/info/info.area.ts';
+import { Log } from './middlwares/log.middlware.ts';
+
+const app = new App({
+  areas: [HomeArea, InfoArea],
+  middlewares: [Log]
+});
+
+app.listen();
---
+++ areas/home/home.controller.ts
@@ -0,0 +1,68 @@
+import {
+  Body,
+  Content,
+  Controller,
+  Cookie,
+  ForbiddenError,
+  Param,
+  Get,
+  Post,
+  QueryParam,
+  Req,
+  Res,
+  Response,
+  ServerRequest
+} from 'alosaur/mod.ts';
+
+@Controller('/home')
+export class HomeController {
+  constructor() {}
+
+  @Get('/text')
+  text(
+    @QueryParam('name') name: string,
+    @QueryParam('test') test: string,
+    @Cookie('username') username: string
+  ) {
+    return Content(`Hello world, ${name} ${test} ${username}`);
+  }
+
+  @Get('/json')
+  json(
+    @Req() request: ServerRequest,
+    @Res() response: Response,
+    @QueryParam('name') name: string
+  ) {
+    return Content(response);
+  }
+
+  @Get('/error')
+  error() {
+    throw new ForbiddenError('error');
+  }
+
+  @Get('/test')
+  gerTests() {
+    return Content('test');
+  }
+
+  @Get('/test/:id')
+  gerParamId(@Param('id') id) {
+    return Content(id);
+  }
+
+  @Get('/test/:id/:name')
+  gerParamIdName(@Param('id') id, @Param('name') name) {
+    return Content(`${id} ${name}`);
+  }
+
+  @Get('/test/:id/:name/detail')
+  gerParamIdNameDetail(@Param('id') id, @Param('name') name) {
+    return Content(`${id} ${name} detail`);
+  }
+
+  @Post('/post')
+  post(@Body() body, @QueryParam('name') name: string) {
+    return Content(body);
+  }
+}
